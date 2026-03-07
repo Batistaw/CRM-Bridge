6b93cc46adf7236ac9b3e6e5d0f680690b192bd3
Batistaw <wgoncalvesbatistaa@gmail.com>
2026-03-07 19:15:20 -0400

    Initial commit: estrutura de pastas front e back

---
+++ backend-crm-bridge/crm-bridge/src/main/java/com/github/batistaw/crmBridge/CrmBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.github.batistaw.crmBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmBridgeApplication.class, args);
+	}
+
+}
---
+++ backend-crm-bridge/crm-bridge/src/test/java/com/github/batistaw/crmBridge/CrmBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.batistaw.crmBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
